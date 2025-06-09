d2be10d7a3c1ab8b1eedc3ded84a386d5a463a3a
oneth sayakkara <onethsayakkara@gmail.com>
2025-06-09 16:15:50 +0530

    about me 40% top section

--- src/components/Aboutme.jsx
+++ src/components/Aboutme.jsx
@@ -3,6 +3,7 @@ import Header from './Header';
 import { GoArrowDownRight } from 'react-icons/go';
 import eyeAnimation from '../assets/animations/e2.json';
 import Lottie from 'lottie-react';
+import { IoMapOutline } from "react-icons/io5";
 
 const Aboutme = () => {
   return (
@@ -42,8 +43,9 @@ const Aboutme = () => {
           <div className="absolute right-[75px] top-0 h-full w-px bg-black scale-x-[0.2]" />
           <div className='relative mt-[1.30rem]'>
             <div className="absolute left-[75px] right-[75px] top-0 overflow-hidden">
-              <div className="absolute right-[610px] top-0 h-full w-px bg-black scale-x-[0.2]" />
+              
               <div className='flex flex-row gap-10'>
+                
                 <div className='flex flex-col w-[54%] font-madefor font-medium text-[0.75rem]'>
                   <button className='h-[70px] border-b border-r border-black/20 uppercase hover:tracking-[0.5rem] transition-[letter-spacing] duration-300 ease-in-out bg-fill-hover'>
                     <span className="relative z-10">who am i</span>
@@ -54,25 +56,55 @@ const Aboutme = () => {
                   <button className='h-[70px] border-r uppercase border-black/20 hover:tracking-[0.5rem] transition-[letter-spacing] duration-300 ease-in-out bg-fill-hover'>
                     <span className="relative z-10">beyond the screen</span>
                   </button>
+                  
                 </div>
                 <div className='font-madefor text-xs text-brightred mt-36'>
                   <p>ALWAYS UP FOR A GAME OF ANY SPORT<br />
                     BUT FOOTBALL IS HIS TRUE PASSION<br />
                     TRAINING AND KICKING BALLS SINCE FOREVER</p>
                 </div>
+                <div className="absolute right-[610px] top-0 h-[230px] w-px bg-black scale-x-[0.2]" />
               </div>
-              <div id='div3' className="absolute bottom-0 left-0 w-full border-t border-black scale-y-[0.2]"></div>
+              <div className="absolute  left-0 w-full border-t border-black scale-y-[0.2]"></div>
+              
               {/* Creative Developer section - positioned below div3 */}
-              <div className="absolute left-0 right-0 bottom-[40px] h-[200px] flex items-center ">
-                <div className="scroll-item text-9xl uppercase font-madefor font-medium text-black flex items-center">
-                  <p className="flex flex-row items-center justify-center w-full gap-3">
-                    Creative Developerhkjkjh
+              <div className="relative left-0 right-0  h-[255px] flex items-center">
+                
+                <div 
+                 className="scroll-about text-[9.4rem] uppercase font-madefor font-medium text-black flex items-center">
+                  <p className="flex flex-row items-center justify-center w-full gap-8">
+                    about me
+                    <IoMapOutline className="text-brightred text-4xl mx-4" />
+                    about me
+                    <IoMapOutline className="text-brightred text-4xl mx-4" />
+                     about me
+                    <IoMapOutline className="text-brightred text-4xl mx-4" />
+                     about me
+                    <IoMapOutline className="text-brightred text-4xl mx-4" />
+                   
                   </p>
+                  
                 </div>
+                
+              </div>
+       
+            </div>
+             <div className="absolute top-[27.5rem] left-0 w-full border-t border-black scale-y-[0.2]"></div>
+             <div className="absolute left-0 top-[27.5rem] w-full border-b border-black/25">
+              <div className="flex flex-row">
+                <div className="justify-center items-center w-fit left-10 ml-14 text-center text-sm font-madefor">
+                  <p className="w-[20px]">1</p> 
+                </div>
+                <div className="w-[1390px] bg-darkgray text-white text-[0.55rem] py-1 px-5 font-madefor uppercase">
+                  who am i
+                </div>
+                <div className="w-fit flex items-center justify-center ml-[2px]"></div>
               </div>
             </div>
+             <div className="absolute top-[229px] w-full border-t border-black scale-y-[0.2]"></div>
           </div>
         </div>
+       
       </section>
     </div>
   );
